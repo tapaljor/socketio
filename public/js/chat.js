23cf66fbcfa0f2b019d47d903526ccde63e67387
Tashi Paljor <tapaljor@gmail.com>
2024-10-05 13:40:42 -0400

    Oct 5 @1340

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,54 +1,123 @@
 var socket = io.connect('http://localhost:3000');
 
-socket.emit('new connection', { username: 'User joined the chat' });
+//receiving msg through socket/server
+socket.on('updatedUser', (data) => {
+	let userList = document.getElementById('user-list');
+	if (userList) { //if logged in then see user list
+		let listItems = userList.getElementsByTagName('li');
 
-//receiving msg through socket
-socket.on('chat', function (data) {
-	let lounge = document.getElementById('lounge');
-	let a = document.createElement('a');//for link
-	a.href = `/${data.username}`;
-	a.innerHTML = `<b>${data.username}:</b> ${data.message}<br/>`;
-	lounge.prepend(a);
+		for (let i = 0; i < listItems.length; i++) {
+			if (listItems[i].textContent == data.username) {
+				listItems[i].remove();
+			}
+		}
+		let li = document.createElement('li');//for link
+		li.innerHTML = `<a href="/to/${data.username}">${data.username}</a>`;
+		userList.prepend(li);
+	}
 });
-
+socket.on('chat', function (data) {
+	let fromm = document.getElementById('fromm');
+	let too = document.getElementById('too');
+	if (fromm && too) {
+		if (fromm.value === data.too && data.fromm === too.value) { //only particular user msg comes here
+			let output = document.getElementById('output');
+			let li = document.createElement('li');//for link
+			li.innerHTML = `<a href="/to/${data.fromm}"><b>${data.fromm}:</b> <i>${data.message}</i></a>`;
+			output.prepend(li);
+		}
+	}
+	// notifications of new msgs
+	let notifications = document.getElementById('notifications');
+	let lin = document.createElement('li');
+	lin.innerHTML = `<a href="/to/${data.fromm}">New message from ${data.fromm}</a>`;
+	notifications.appendChild(lin);
+});
 socket.on('typing', function (data) {
 	let typingMsg = document.getElementById('actions');
-	typingMsg.innerHTML = `<i>${data} is typing...</i>`;
+	typingMsg.innerHTML = `<i>${data.fromm} is typing...</i>`;
 	setTimeout(() => {
 		typingMsg.innerHTML = '...';
 	}, 2000);  // Delay of 2000 milliseconds (2 seconds)
 });
-socket.on('updateUsers', (data) => {
-	console.log(`got message from server emit ${data}` );
-	let users = document.getElementById('chat-left');
-	data.users.forEach((user) => {
-		let a = document.createElement('a');//for link
-		a.href = `/${data.socketId}`;
-		a.textContent = `${data.username}`;
-		users.prepend(a);
-	});
+socket.on('registrationDone', (data) => {
+	if (data.success == true) {
+		window.location.href = `/index/${data.username}`;
+	} else {
+		console.log(data.message);
+	}
 });
-/*function sendMessage() {
-	var username = document.getElementById('username').value;
-	var message = document.getElementById('message').value;
+/*socket server msg receiving ends */
 
-	socket.emit('chat', { username: username, message: message });
-	document.getElementById('message').value = '';  // Clear input field
+// from the server side to server
+let sendMessage = document.getElementById('sendMessage');
+var messageInput = document.getElementById('message');
+
+if (sendMessage || messageInput) {
+	sendMessage.addEventListener('click', (e) => {
+		e.preventDefault();
+		sendMessageToServer();
+	});
+	messageInput.addEventListener('keydown', (e) => {
+		if (e.key === 'Enter') {
+			sendMessageToServer();
+		}
+	});
 }
-document.getElementById('message').addEventListener('keypress', function () {
-	var username = document.getElementById('username').value;
-	socket.emit('typing', username);
-});*/
+function sendMessageToServer() {
+	const output = document.getElementById('output');
+	var fromm = document.getElementById('fromm').value;
+	var too = document.getElementById('too').value;
+	var message = messageInput.value;
+	socket.emit('chat', { fromm: fromm, too: too, message: message });
 
+	//message for yourself
+	let li = document.createElement('li');
+	li.textContent = `me: ${messageInput.value}`;
+	output.prepend(li);
+	messageInput.value = '';  // Clear input field
+}
 
-// starging chat
-let userJoin = document.getElementById('userJoin');
+/*sending broading where the message is typing*/
+let messageTyping = document.getElementById('message');
+if (messageTyping) {
+	messageTyping.addEventListener('keypress', function () {
+		var fromm = document.getElementById('fromm').value;
+		socket.emit('typing', { fromm: fromm });
+	});
+}
+
+// starting chat, sending to server's socket to do necessay registertion
+let userJoinButton = document.getElementById('userJoin');
 let usernameInput = document.getElementById('username');
-if (userJoin) {
-	userJoin.addEventListener('click', (e) => {
+let roomInput = document.getElementById('room');
+if (userJoinButton) {
+	userJoinButton.addEventListener('click', (e) => {
 		e.preventDefault();
 		const username = usernameInput.value;
-		socket.emit('userJoin', { username: username });
+		if (username) {
+			socket.emit('userJoin', { username: username, room: roomInput.value });
+		} else {
+			alert('Fields cannot be empty.');
+		}
 	});
 }
 
+// selecting users based on room
+let roomSelect = document.getElementById('room');
+let userList = document.getElementById('user-list');
+if (roomSelect) {
+	roomSelect.addEventListener('change', (e) => {
+		e.preventDefault();
+		fetch(`/fetchUsersByRoom/${roomSelect.value}`)
+			.then(response => response.json())
+			.then(users => {
+				userList.innerHTML = '';
+				for (let a = 0; a < users.length; a++) {
+					let li = document.createElement('li');//for link
+					li.innerHTML = `<a href="/to/${users[a].username}">${users[a].username}</a>`;
+					userList.prepend(li);
+				}
+			});
+	});
+}
--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require('express');
 var socket = require('socket.io');
 var path = require('path');
 var exphbs = require("express-handlebars");
-
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const ioSession = require("express-socket.io-session");
 const { MongoClient } = require("mongodb");
 
 const uri = "mongodb+srv://tapaljor:A81Z6ZvQjnhud1Xx@tpcluster.pmqosjh.mongodb.net/?retryWrites=true&w=majority&appName=TPCluster";
@@ -18,7 +20,6 @@ async function connectDB() {
         process.exit(1); // Exit if there is a connection error
     }
 }
-
 var app = express();
 
 // Serve static files
@@ -35,67 +36,168 @@ app.engine(".hbs", exphbs.engine({
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "/views"));
 
-app.use(express.urlencoded({ extended: true }));
+// Use cookie-parser
+app.use(cookieParser());
+
+// Session middleware
+const sessionMiddleware = session({
+    secret: "#WFDSO*&(jHUIL38slds3IIH",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        sameSite: 'Lax', 
+        secure: false,
+        maxAge: 600000
+    }
+});
+
+// session middleware in express
+app.use(sessionMiddleware);
 
 // Initiate server and database connection
 (async () => {
     const db = await connectDB(); // Await database connection
 
+    //for developing resetting collection to 0
+    //await db.collection("chaUsers").deleteMany({});
+
     // Store the database connection in app.locals for access across the app
     app.locals.db = db;
+
     // Define routes
-    app.get("/", (req, res) => {
-        res.render('index');
+    app.get("/", async (req, res) => {
+        try {
+            const rooms = await db.collection("chaRooms").find({}).toArray();
+            res.render('index', { rooms });
+        } catch (err) {
+            console.error(err);
+        }
     });
-    app.get("/:to", (req, res) => {
-        console.log(req.params.to);
-        res.render('private', { to: req.params.to });
+    // setting session
+    app.get("/index/:username", async (req, res) => {
+        req.session.username = req.params.username; //setting up session
+        try {
+            const users = await db.collection("chaUsers").find({}).toArray();
+            const rooms = await db.collection("chaRooms").find({}).toArray();
+            res.render('users', { users, rooms });
+        } catch (err) {
+            console.error(err);
+        }
     });
-    app.post("/start", async (req, res) => {
-        console.log(req.body);
+    app.get("/users", async (req, res) => {
+        try {
+            if (req.session.username) {
+                const users = await db.collection("chaUsers").find({}).toArray();
+                const rooms = await db.collection("chaRooms").find({}).toArray();
+                res.render('users', { users, rooms });
+            } else {
+                const rooms = await db.collection("chaRooms").find({}).toArray();
+                res.render('index', { rooms });
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    });
+    app.get("/fetchUsersByRoom/:room", async (req, res) => {
+        try {
+            const users = await db.collection("chaUsers").find({
+                room: req.params.room
+            }).toArray();
+            res.json(users);
+        } catch (err) {
+            console.error(err);
+        }
+    });
+    app.get("/to/:too", async (req, res) => {
+        try {
+            const users = await db.collection("chaUsers").find({}).toArray();
+            res.render('index', { users, fromm: req.session.username, too: req.params.too });
+        } catch (err) {
+            console.error(err);
+        }
     });
     // Start the server
-    var server = app.listen(3000, () => {
+    var server = app.listen(3000, function () {
         console.log('Server running on port 3000');
     });
-
     // Attach socket.io to the server
     var io = socket(server);
 
+    // Use the session middleware in Socket.io
+    io.use(ioSession(sessionMiddleware, {
+        autoSave: true  // Save the session automatically after each request
+    }));
+
     // Socket.io connection
     io.on('connection', function (socket) {
-        console.log('A user connected');
 
-        socket.on('disconnect', async() => {
-            try {
-             /*   let result = await db.collection("chaUsers").deleteOne(
-                    {socketId: socket.id}
-                );
-                if ( result) {
-                    users = await db.collection("chaUsers").find({});
-                    io.emit('updateUsers', { users });
-                }*/
-            } catch(err) {
-                console.log(err);
-            }
-        });
+        //accessing session into websocket
+        const session = socket.handshake.session;
+
+        console.log('socket id refreshed ', socket.id);
+
+        // update the socket id when users refreshes
+        if (session.username) {
+            db.collection('chaUsers').updateOne(
+                { username: session.username },
+                { $set: { socketId: socket.id } },
+                { upsert: true }
+            ).then(() => {
+                console.log(`Socket ID ${socket.id} updated for user ${session.username}`);
+                io.emit('updatedUser', { username: session.username, socketId: socket.id });
+            }).catch(err => {
+                console.error('Error updating socket ID:', err);
+            });
+            socket.on('disconnect', function () {
+                console.log('A user disconnected');
+            });
+            socket.on('chat', async (msg) => {
+                try {
+                    const user = await db.collection("chaUsers").findOne(
+                        { username: msg.too },
+                        { projection: { socketId: 1 } }
+                    );
+                    if (user) {
+                        io.to(user.socketId).emit('chat', msg);
+                    }
+                } catch (err) {
+                    console.error(err);
+                }
+            });
+            socket.on('typing', (msg) => {
+                io.emit('typing', msg);
+            });
+        }
         socket.on('userJoin', async (msg) => {
-            const data = {socketId: socket.id, username: msg.username }
+            const data = { socketId: socket.id, username: msg.username, room: msg.room }
             try {
-                const user = await db.collection('chaUsers').insertOne(
-                   data
+                const result = await db.collection('chaUsers').findOne(
+                    { username: msg.username },
+                    { projection: { username: 1 } }
                 );
-            } catch(err) {
+                if (result) { // if already registered, update with newly selected room
+                    try {
+                        const updatedUser = await db.collection('chaUsers').updateOne(
+                            { username: msg.username },
+                            { $set: { room: msg.room } }
+                        );
+                        if (updatedUser) {
+                            socket.emit('registrationDone', { success: true, username: msg.username, message: "Registration done." });
+                        }
+                    } catch (err) {
+                        console.error(err);
+                    }
+                } else { //if the user is not registered, then register
+                    const user = await db.collection('chaUsers').insertOne(data);
+                    if (user) {
+                        socket.emit('registrationDone', { success: true, username: msg.username, message: "Registration done." });
+                    } else {
+                        socket.emit('registrationDone', { success: false, message: "Registration not done." });
+                    }
+                }
+            } catch (err) {
                 console.log(err);
             }
-            io.emit('userJoin', data);
-        });
-        socket.on('chat', (msg) => {
-            io.emit('chat', msg); // Broadcast the message to all clients
-        });
-
-        socket.on('typing', (msg) => {
-            io.emit('typing', msg);
         });
     });
 })();
